d986e9f040a5bb6d31a6820387d86b7005a718b7
emilychen <emilymchen@gmail.com>
2020-03-06 10:10:35 -0500

    Add drawings and bagel

---
+++ TeamCode/src/Bagel.java
@@ -0,0 +1,6 @@
+public class Bagel extends Snack {
+    private final int POINT_VAL = 100;
+    public Bagel() {
+        super("Pass-Man Drawings/bagel.png", 100);
+    }
+}
--- TeamCode/src/Student.java
+++ TeamCode/src/Student.java
@@ -1,2 +1,5 @@
 public class Student extends Character {
+    public Student(int x, int y, int w, int h, int v) {
+        super(x,y,w,h,v);
+    }
 }
