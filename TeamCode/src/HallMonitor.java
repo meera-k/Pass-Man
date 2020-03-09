637fe1ba3a44e8c0cfb51e8950d2c8a4cd28255c
emilychen <emilymchen@gmail.com>
2020-03-09 10:49:25 -0400

    Add sharon

--- TeamCode/src/HallMonitor.java
+++ TeamCode/src/HallMonitor.java
@@ -1,11 +1,22 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class HallMonitor extends Character {
+    private String imagePath = "graphics/sharon.png";
+    /*try {
+        BufferedImage image = ImageIO.read(new File(imagePath));
+    } catch(IOException e) {
+
+    }*/
     public HallMonitor(int x, int y, int w, int h, int v) {
         super(x,y,w,h,v);
     }
 
     public void draw(Graphics g) {
-        
+        g.drawImage(new ImageIcon(imagePath).getImage(),getX(),getY(),null);
     }
 }
