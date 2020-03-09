17fd7b0a1daaa9ab287ae9d673aff65569aa27e6
emilychen <emilymchen@gmail.com>
2020-03-09 11:17:48 -0400

    get rid of unnecessary imports

--- TeamCode/src/HallMonitor.java
+++ TeamCode/src/HallMonitor.java
@@ -1,20 +1,6 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.awt.Container;
-import java.awt.EventQueue;
 import java.awt.Image;
-import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
-import javax.swing.JFrame;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
-import javax.swing.JLabel;
-
-import static com.sun.javafx.iio.common.ImageTools.scaleImage;
 
 public class HallMonitor extends Character {
     private String imagePath = "graphics/sharon.png";
