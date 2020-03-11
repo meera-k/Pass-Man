18e68bfd14609224112989eadb3338e0afcdb891
Sydney-tran <sydney.tran@live.com>
2020-03-11 12:58:10 -0400

    AHS map

--- TeamCode/src/Map.java
+++ TeamCode/src/Map.java
@@ -6,9 +6,9 @@ public class Map {
     private int width, height;
 
     public Map(int w, int h) {
-        grid = new Location[19][13];
-        numRows = 19;
-        numCols = 13;
+        grid = new Location[13][19];
+        numRows = 13;
+        numCols = 19;
         width = w / 19;
         height = h / 13;
 
@@ -21,19 +21,62 @@ public class Map {
         // set border
         for (int col = 0; col < numCols; col++) {
             grid[0][col].setWall();
-            grid[18][col].setWall();
+            grid[12][col].setWall();
         }
         for (int row = 0; row < numRows; row++) {
                 grid[row][0].setWall();
-                grid[row][12].setWall();
+                grid[row][18].setWall();
         }
+        // set other walls
+        for (int col = 2; col <= 16; col++) {
+            if (col != 5 && col != 9 && col != 13) {
+                grid[2][col].setWall();
+                grid[10][col].setWall();
+            }
+        }
+        for (int row = 2; row <= 10; row++) {
+            if (row != 4 && row != 8) {
+                grid[row][2].setWall();
+                grid[row][16].setWall();
+            }
+        }
+
+        // set AHS
+        for (int col = 4; col <= 14; col++) {
+            if (col != 7 && col != 9 && col != 11) {
+                grid[4][col].setWall();
+            }
+        }
+        for (int col = 4; col <= 12; col++) {
+            if (col != 5 && col != 7 && col != 9 && col != 11) {
+                grid[5][col].setWall();
+            }
+        }
+        for (int col = 4; col <= 14; col++) {
+            if (col != 7 && col != 11) {
+                grid[6][col].setWall();
+            }
+        }
+        for (int col = 4; col <= 14; col++) {
+            if (col != 5 && col != 7 && col != 9 && col != 11 && col != 12 && col != 13) {
+                grid[7][col].setWall();
+            }
+        }
+        for (int col = 4; col <= 14; col++) {
+            if (col != 5 && col != 7 && col != 9 && col != 11) {
+                grid[8][col].setWall();
+            }
+        }
+
+
+
 
     }
 
     public void draw(Graphics g) {
         for (int row = 0; row < numRows; row++) {
             for (int col = 0; col < numCols; col++) {
-                grid[row][col].draw(g, row * width, col * height, width, height);
+                grid[row][col].draw(g, col * width, row * height, width, height);
             }
         }
     }
