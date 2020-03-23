909e14bd16cf910ec0c1ce51c72c02d989dc28f4
Sydney-tran <sydney.tran@live.com>
2020-03-22 20:48:22 -0400

    put color change for power back

--- TeamCode/src/UserPanel.java
+++ TeamCode/src/UserPanel.java
@@ -355,7 +355,11 @@ public class UserPanel extends JPanel implements KeyListener, ActionListener, Ja
         ImageIcon bagel = new ImageIcon("graphics/bagel.png");
         g.drawImage(bagel.getImage(),100,100,null);
 
-        map.draw(g);
+        if (power) {
+            map.draw(g, Color.GREEN);
+        } else {
+            map.draw(g, Color.CYAN);
+        }
 
         for (Dot d : dots) {
             d.draw(g);
